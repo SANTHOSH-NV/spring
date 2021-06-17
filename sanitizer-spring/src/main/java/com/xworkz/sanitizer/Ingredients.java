c727969743930043216cb5a9e236360b97f31d74
SANTHOSH-NV <78070806+SANTHOSH-NV@users.noreply.github.com>
2021-06-17 21:52:19 +0530

    Add files via upload

--- sanitizer-spring/src/main/java/com/xworkz/sanitizer/Ingredients.java
+++ sanitizer-spring/src/main/java/com/xworkz/sanitizer/Ingredients.java
@@ -1,5 +1,8 @@
 package com.xworkz.sanitizer;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
@@ -7,25 +10,33 @@ import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
+@Component
 @Setter
 @Getter
 @NoArgsConstructor
 @ToString
 @RequiredArgsConstructor
 
-
 public class Ingredients {
-	
+
 	@NonNull
+	@Value("Isopropyl Alcohol")
 	private String alcohol;
+
 	@NonNull
+	@Value("79")
 	private double alcoholPercentage;
+
 	@NonNull
+	@Value("Hydrogen Peroxide")
 	private String peroxide;
+
 	@NonNull
+	@Value("0.125")
 	private double peroxidePercentage;
+
 	@NonNull
+	@Value("20")
 	private double distilledWaterPercentage;
-	
 
 }
--- sanitizer-spring/src/main/java/com/xworkz/sanitizer/Sanitizer.java
+++ sanitizer-spring/src/main/java/com/xworkz/sanitizer/Sanitizer.java
@@ -1,12 +1,16 @@
 package com.xworkz.sanitizer;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
-
+@Component
 @Setter
 @Getter
 @ToString
@@ -14,22 +18,29 @@ import lombok.ToString;
 @RequiredArgsConstructor
 
 public class Sanitizer {
+	
 	@NonNull
+	@Value("Lifebouy")
 	private String name;
 	
 	@NonNull
+	@Value("200ML")
 	private String quantity;
 	
 	@NonNull
+	@Value("Blue")
 	private String color;
 	
 	@NonNull
+	@Value("200")
 	private double price;
 	
 	@NonNull
+	@Autowired
 	private Ingredients ingredients;
 	
 	@NonNull
+	@Value("False")
 	private boolean isAyurvedic;
 	
 	
